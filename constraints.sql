a50c26e5bcb9548b3d05ac1ca93c55d3caa4c796
kodekill <brian.falor@gmail.com>
2017-11-14 20:38:46 -0700

    Update constraints.sql
    Added 2 and 7

--- constraints.sql
+++ constraints.sql
@@ -1,10 +1,20 @@
 -- 1
-
 ALTER TABLE public.batting ALTER COLUMN ab SET DEFAULT 20;
 
+-- 2
+ALTER TABLE public.batting 
+        ADD CONSTRAINT batting_constraint CHECK (h > ab);
 
 -- 3
 ALTER TABLE public.teams
-        ADD CONSTRAINT league_constraint CHECK (lgid IN ('NL','AL'))
+        ADD CONSTRAINT league_constraint CHECK (lgid IN ('NL','AL'));
+
+-- 4
 
 -- 5
+
+-- 6
+
+-- 7
+ALTER TABLE public.master
+	ADD CONSTRAINT nameConstraint UNIQUE (namefirst, namelast); 
